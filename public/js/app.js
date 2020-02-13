fb3d08b2058745e32af245a10bb42ae03991d03a
Vishalraj <vishalraj.elluminati@gmail.com>
2020-02-13 14:34:17 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+
+    // Fetching forecast
+
+    
+
+    const weatherForm = document.querySelector('form')
+    const search = document.querySelector('input')
+    const messageOne = document.getElementById('message-1')
+    const messageTwo = document.getElementById('message-2')
+    
+    //messageOne.textContent = 'From Javascript'
+
+    weatherForm.addEventListener('submit',(e)=>{
+        e.preventDefault()
+
+        const location = search.value
+
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+
+      
+        fetch('http://localhost:3000/weather?address='+location).then((response) =>{
+        response.json().then((data)=>{
+            if(data.error)
+            {
+                messageOne.textContent = data.error
+                messageTwo.textContent = ''
+
+            }
+            else{
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+            
+        })
+    })
+    })  
---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+
+const forecast =(latitude,longitude,callback) =>{
+    
+    const url = 'https://api.darksky.net/forecast/e926a0b6c32fb7c83675d58453ff50ed/'+ latitude +','+ longitude
+    request({ url, json: true},(error, {body}) =>{
+
+        if(error){
+            callback('Unable to connect to weather service!(Please check your internet connection and try again.)',undefined)
+        }
+        else if(body.error){
+            callback('Unable to fetch  Location',undefined)
+        }
+        
+        else{
+            callback(undefined,body.daily.data[0].summary  +'It is currently '+body.currently.temperature +' degrees out. There is a '+body.currently.precipProbability+'% chance of rain')       
+        }
+    })
+}
+
+module.exports = forecast
